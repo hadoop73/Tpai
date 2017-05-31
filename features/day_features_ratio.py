5158b0d4dca07c04990d14e5df1f7c8fbcac3bde
Bean <python35@163.com>
2017-05-31 09:41:40 +0800

    multi day feas

--- features/day_features_ratio.py
+++ features/day_features_ratio.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 
 
-train = pd.read_csv('../data/dup/train_count.csv')
-train.drop(['conversionTime','recode_str'],axis=1,inplace=True)
+train = pd.read_csv('../data/pre/train.csv')
+train.drop(['conversionTime'],axis=1,inplace=True)
 
 test = pd.read_csv('../data/dup/test.csv')
 test.drop('instanceID',axis=1,inplace=True)
@@ -31,14 +31,14 @@ d.loc[:,'clickTime'] = d['clickTime'].apply(lambda x:int(x/10000))
 d.loc[:,'hometown'] = d['hometown'].apply(lambda x:int(x/100))
 d.loc[:,'residence'] = d['residence'].apply(lambda x:int(x/100))
 
-d = d[d['clickTime']>=21]
+d = d[d['clickTime']>=19]
 
 cols = ['gender','education','marriageStatus','haveBaby','hometown','residence','sitesetID',
         'adID','camgaignID','appID','appPlatform','creativeID','advertiserID','positionID','positionType',
         'connectionType','telecomsOperator']
 import gc
 
-#cols = ['gender']
+cols = ['gender']
 def writeCols(col):
     t = d[['label','clickTime',col]]
     t = t.groupby([col,'clickTime'],as_index=False)['label'].agg({col+"day_ratio":np.mean,
@@ -57,41 +57,52 @@ pool.map(writeCols,cols)
 pool.close()
 pool.join()
 
-for col in cols:
-    t = pd.read_csv('../data/dup/{}_day_ratio.csv'.format(col))
-    for i in range(3):
-        t.loc[:,'clickTime'] = t['clickTime'] + 1
-        d = d.merge(t,on=[col,'clickTime'],how='left')
-        d.rename(columns={col+"day_ratio":col+str(i+1)+"day_ratio"},inplace=True)
-        d.rename(columns={col + "day_Pcount": col + str(i + 1) + "day_Pcount"}, inplace=True)
-        gc.collect()
-    d.loc[:,col+"day_2ratio"] = d[[col+"1day_ratio",col+"2day_ratio"]].apply(np.mean,axis=1)
-    d.loc[:,col+"day_2Pcount"] = d[[col+"1day_Pcount",col+"2day_Pcount"]].apply(np.mean,axis=1)
+def delPart(dt):
+    for col in cols:
+        t = pd.read_csv('../data/dup/{}_day_ratio.csv'.format(col))
+        for i in range(3):
+            t.loc[:,'clickTime'] = t['clickTime'] + 1
+            dt = dt.merge(t,on=[col,'clickTime'],how='left')
+            dt.rename(columns={col+"day_ratio":col+str(i+1)+"day_ratio"},inplace=True)
+            dt.rename(columns={col + "day_Pcount": col + str(i + 1) + "day_Pcount"}, inplace=True)
+            gc.collect()
 
-    d.loc[:,col+"day_3ratio"] = d[[col+"1day_ratio",col+"2day_ratio",col+"3day_ratio"]].apply(np.mean,axis=1)
-    d.loc[:,col+"day_3Pcount"] = d[[col+"1day_Pcount",col+"2day_Pcount",col+"3day_Pcount"]].apply(np.mean,axis=1)
+        dt.loc[:,col+"day_2ratio"] = dt[[col+"1day_ratio",col+"2day_ratio"]].apply(np.mean,axis=1)
+        dt.loc[:,col+"day_2Pcount"] = dt[[col+"1day_Pcount",col+"2day_Pcount"]].apply(np.mean,axis=1)
 
-    t = pd.read_csv('../data/dup/{}_hour_ratio.csv'.format(col))
+        dt.loc[:,col+"day_3ratio"] = dt[[col+"1day_ratio",col+"2day_ratio",col+"3day_ratio"]].apply(np.mean,axis=1)
+        dt.loc[:,col+"day_3Pcount"] = dt[[col+"1day_Pcount",col+"2day_Pcount",col+"3day_Pcount"]].apply(np.mean,axis=1)
 
-    for i in range(3):
-        t.loc[:, 'clickTime'] = t['clickTime'] + 1
-        d = d.merge(t, on=[col, 'clickTime', 'hour'], how='left')
-        d.rename(columns={col+"hour_ratio": col + str(i + 1) + "hour_ratio"}, inplace=True)
-        d.rename(columns={col+"hour_Pcount": col + str(i + 1) + "hour_Pcount"}, inplace=True)
+        t = pd.read_csv('../data/dup/{}_hour_ratio.csv'.format(col))
 
-        gc.collect()
+        for i in range(3):
+            t.loc[:, 'clickTime'] = t['clickTime'] + 1
+            dt = dt.merge(t, on=[col, 'clickTime', 'hour'], how='left')
+            dt.rename(columns={col+"hour_ratio": col + str(i + 1) + "hour_ratio"}, inplace=True)
+            dt.rename(columns={col+"hour_Pcount": col + str(i + 1) + "hour_Pcount"}, inplace=True)
 
-    d.loc[:,col+"hour_2ratio"] = d[[col+"1hour_ratio",col+"2hour_ratio"]].apply(np.mean, axis=1)
-    d.loc[:,col+"hour_2Pcount"] = d[[col+"1hour_Pcount",col+"2hour_Pcount"]].apply(np.mean, axis=1)
+            gc.collect()
 
-    d.loc[:,col+"hour_3ratio"] = d[[col+"1hour_ratio",col+"2hour_ratio",col+"3hour_ratio"]].apply(np.mean,
-                                                                                                         axis=1)
-    d.loc[:,col+"hour_3Pcount"] = d[[col+"1hour_Pcount",col+ "2hour_Pcount", col + "3hour_Pcount"]].apply(np.mean,
+        dt.loc[:,col+"hour_2ratio"] = dt[[col+"1hour_ratio",col+"2hour_ratio"]].apply(np.mean, axis=1)
+        dt.loc[:,col+"hour_2Pcount"] = dt[[col+"1hour_Pcount",col+"2hour_Pcount"]].apply(np.mean, axis=1)
+
+        dt.loc[:,col+"hour_3ratio"] = dt[[col+"1hour_ratio",col+"2hour_ratio",col+"3hour_ratio"]].apply(np.mean,
+                                                                                           axis=1)
+        dt.loc[:,col+"hour_3Pcount"] = dt[[col+"1hour_Pcount",col+ "2hour_Pcount", col + "3hour_Pcount"]].apply(np.mean,
                                                                                                              axis=1)
+        return dt
 
+dts = [d[d['clickTime']==i] for i in range(22,32)]
+
+pool = Pool(4)
+rst = pool.map(delPart,dts)
+pool.close()
+pool.join()
+
+d = pd.concat(rst)
 
 from train_sample import dataSampleDay,dataTrain
-train,valid,test = dataSampleDay(d,rate=0.25)
+train,valid,test = dataSampleDay(d,rate=0.1)
 
 print train.head()
 print train.shape,valid.shape,test.shape
