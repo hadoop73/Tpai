6ec245f2780a78053fa31450952a5286f6f6d011
jqk2017 <2034263349@qq.com>
2017-05-31 08:56:19 +0800

    day fea

--- features/day_features_ratio.py
+++ features/day_features_ratio.py
@@ -5,8 +5,8 @@ import pandas as pd
 import numpy as np
 
 
-train = pd.read_csv('../data/dup/train.csv')
-train.drop('conversionTime',axis=1,inplace=True)
+train = pd.read_csv('../data/dup/train_count.csv')
+train.drop(['conversionTime','recode_str'],axis=1,inplace=True)
 
 test = pd.read_csv('../data/dup/test.csv')
 test.drop('instanceID',axis=1,inplace=True)
@@ -38,33 +38,68 @@ cols = ['gender','education','marriageStatus','haveBaby','hometown','residence',
         'connectionType','telecomsOperator']
 import gc
 
-for col in cols:
+#cols = ['gender']
+def writeCols(col):
     t = d[['label','clickTime',col]]
-    t = t.groupby([col,'clickTime'],as_index=False)['label'].mean()
-    t.rename(columns={'label':col+"day_ratio"},inplace=True)
+    t = t.groupby([col,'clickTime'],as_index=False)['label'].agg({col+"day_ratio":np.mean,
+                                                                  col + "day_Pcount":np.sum})
+    t.to_csv('../data/dup/{}_day_ratio.csv'.format(col),index=None)
+
+    t = d[['label', 'clickTime', 'hour', col]]
+    t = t.groupby([col, 'clickTime', 'hour'], as_index=False)['label'].agg({col+"hour_ratio":np.mean,
+                                                                    col + "hour_Pcount":np.sum})
+    t.to_csv('../data/dup/{}_hour_ratio.csv'.format(col),index=None)
+
+
+from multiprocessing import Pool
+pool = Pool(4)
+pool.map(writeCols,cols)
+pool.close()
+pool.join()
+
+for col in cols:
+    t = pd.read_csv('../data/dup/{}_day_ratio.csv'.format(col))
     for i in range(3):
         t.loc[:,'clickTime'] = t['clickTime'] + 1
         d = d.merge(t,on=[col,'clickTime'],how='left')
         d.rename(columns={col+"day_ratio":col+str(i+1)+"day_ratio"},inplace=True)
+        d.rename(columns={col + "day_Pcount": col + str(i + 1) + "day_Pcount"}, inplace=True)
         gc.collect()
+    d.loc[:,col+"day_2ratio"] = d[[col+"1day_ratio",col+"2day_ratio"]].apply(np.mean,axis=1)
+    d.loc[:,col+"day_2Pcount"] = d[[col+"1day_Pcount",col+"2day_Pcount"]].apply(np.mean,axis=1)
 
-    t = d[['label', 'clickTime', 'hour', col]]
-    t = t.groupby([col, 'clickTime', 'hour'], as_index=False)['label'].mean()
-    t.rename(columns={'label':col+"day_hour_ratio"},inplace=True)
+    d.loc[:,col+"day_3ratio"] = d[[col+"1day_ratio",col+"2day_ratio",col+"3day_ratio"]].apply(np.mean,axis=1)
+    d.loc[:,col+"day_3Pcount"] = d[[col+"1day_Pcount",col+"2day_Pcount",col+"3day_Pcount"]].apply(np.mean,axis=1)
+
+    t = pd.read_csv('../data/dup/{}_hour_ratio.csv'.format(col))
+
     for i in range(3):
         t.loc[:, 'clickTime'] = t['clickTime'] + 1
         d = d.merge(t, on=[col, 'clickTime', 'hour'], how='left')
-        d.rename(columns={col+"day_hour_ratio": col + str(i + 1) + "day_hour_ratio"}, inplace=True)
+        d.rename(columns={col+"hour_ratio": col + str(i + 1) + "hour_ratio"}, inplace=True)
+        d.rename(columns={col+"hour_Pcount": col + str(i + 1) + "hour_Pcount"}, inplace=True)
+
         gc.collect()
 
+    d.loc[:,col+"hour_2ratio"] = d[[col+"1hour_ratio",col+"2hour_ratio"]].apply(np.mean, axis=1)
+    d.loc[:,col+"hour_2Pcount"] = d[[col+"1hour_Pcount",col+"2hour_Pcount"]].apply(np.mean, axis=1)
+
+    d.loc[:,col+"hour_3ratio"] = d[[col+"1hour_ratio",col+"2hour_ratio",col+"3hour_ratio"]].apply(np.mean,
+                                                                                                         axis=1)
+    d.loc[:,col+"hour_3Pcount"] = d[[col+"1hour_Pcount",col+ "2hour_Pcount", col + "3hour_Pcount"]].apply(np.mean,
+                                                                                                             axis=1)
+
+
 from train_sample import dataSampleDay,dataTrain
-train,valid,test = dataTrain(d,rate=0.25)
+train,valid,test = dataSampleDay(d,rate=0.25)
 
 print train.head()
 print train.shape,valid.shape,test.shape
 
-train.to_csv('../data/dup/train_xgbD133.csv',index=None)
-valid.to_csv('../data/dup/valid_xgbD133.csv',index=None)
-test.to_csv('../data/dup/test_xgbD133.csv',index=None)
+#train.to_csv('../data/dup/train_xgbD133.csv',index=None)
+
+train.to_csv('../data/dup/train_ratio.csv',index=None)
+valid.to_csv('../data/dup/valid_ratio.csv',index=None)
+test.to_csv('../data/dup/test_ratio.csv',index=None)
 
 
