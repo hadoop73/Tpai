fb9a35c2bdd7ce9fa81a7053181938dd5a6a6968
jqk2017 <2034263349@qq.com>
2017-06-01 10:39:57 +0800

    feas dummy

--- model/xgbB.py
+++ model/xgbB.py
@@ -7,6 +7,7 @@ import xgboost as xgb
 import numpy as np
 from sklearn import metrics
 import scipy as sp
+import zipfile
 
 def logloss(act, pred):
   epsilon = 1e-15
@@ -24,16 +25,16 @@ def evalerror(preds,dtrain):
 """"""
 #atrain = pd.read_csv('../data/dup/train_xgbA.csv')
 
-atrain = pd.read_csv('../data/dup/train_xgb11U.csv')
-atrain.fillna(-10,inplace=True)
+atrain = pd.read_csv('../data/dup/train_d.csv')
+atrain.fillna(0,inplace=True)
 trainy = atrain['label']
 atrain.drop('label',axis=1,inplace=True)
 
-test = pd.read_csv('../data/dup/test_xgb11U.csv')
-test.fillna(0,inplace=True)
+test = pd.read_csv('../data/dup/test_d.csv')
+test.fillna(-10,inplace=True)
 test.drop('label',axis=1,inplace=True)
 
-avalid = pd.read_csv('../data/dup/valid_xgb11U.csv')
+avalid = pd.read_csv('../data/dup/valid_d.csv')
 avalid.fillna(-10,inplace=True)
 
 validy = avalid['label']
@@ -56,12 +57,12 @@ avalid.drop('label',axis=1,inplace=True)
 
 """
 def XGBoost_(train=atrain,y=trainy,test=test,valid=avalid,validy=validy,k=0,num_round=50,
-			 gamma=1,min_child_weight=0.8,max_depth=8,lamda=15,scale_pos_weight=40,
-			 subsamp=0.9,col_bytree=0.9,col_bylevel=0.62,eta=0.02,file="aac"):
+			 gamma=.1,min_child_weight=5,max_depth=9,lamda=29,scale_pos_weight=40,
+			 subsamp=0.786,col_bytree=0.876,col_bylevel=0.80,eta=0.184,file="aac"):
     param = {'booster': 'gbtree',
              'objective': 'binary:logistic',
              # 'eval_metric':'auc',
-             'scale_pos_weight':int(scale_pos_weight),
+             #'scale_pos_weight':int(scale_pos_weight),
              'gamma': gamma,
              'min_child_weight': min_child_weight,
              'max_depth': int(max_depth),
@@ -80,7 +81,7 @@ def XGBoost_(train=atrain,y=trainy,test=test,valid=avalid,validy=validy,k=0,num_
     #del valid
     dtest = xgb.DMatrix(test, missing=-10)
     #del test
-    watchlist = [(dvalid, 'eval')]
+    watchlist = [(dtrain, 'train'),(dvalid, 'eval')]
     # auc = cv_log['test-auc-mean'].max()
     bst = xgb.train(param, dtrain, num_round, watchlist,early_stopping_rounds=10)
     # make prediction
@@ -94,8 +95,14 @@ def XGBoost_(train=atrain,y=trainy,test=test,valid=avalid,validy=validy,k=0,num_
     #del train, valid
     #del dtrain,dvalid
 
-    treeIndex = bst.predict(dtest, ntree_limit=bst.best_ntree_limit,pred_leaf=True)
-    #p = bst.predict(dtrain, ntree_limit=bst.best_ntree_limit)
+    preds = bst.predict(dtest, ntree_limit=bst.best_ntree_limit)
+    t = pd.read_csv('../data/dup/test.csv')
+    t['prob'] = preds
+    t[['instanceID', 'prob']].to_csv('./submission.csv', index=None)
+    with zipfile.ZipFile('submission1023.zip', 'w') as fout:
+        fout.write('submission.csv', compress_type=zipfile.ZIP_DEFLATED)
+
+    treeIndex = bst.predict(dtest, ntree_limit=bst.best_ntree_limit, pred_leaf=True)
     df = pd.DataFrame(treeIndex)
     df.to_csv('../data/dup/test.xgb.csv', index=None)
 
@@ -134,55 +141,6 @@ def XGBoost_(train=atrain,y=trainy,test=test,valid=avalid,validy=validy,k=0,num_
 
 XGBoost_()
 
-
-trainall = pd.read_csv('../data/dup/train_all_xgb.csv')
-y = trainall['label']
-trainall.drop('label',axis=1,inplace=True)
-
-avalid = pd.read_csv('../data/dup/valid_xgb.csv')
-vy = avalid['label']
-avalid.drop('label',axis=1,inplace=True)
-
-def predict(trainall=trainall,avalid=avalid):
-    bst = xgb.Booster({'nthread': 8})
-    bst.load_model('./bst117.model')
-    dtrain = xgb.DMatrix(trainall)
-    probdf = bst.predict(dtrain,ntree_limit=94,pred_leaf=True)
-
-    dvalid = xgb.DMatrix(avalid)
-    vdf = bst.predict(dvalid,ntree_limit=94, pred_leaf=True)
-    del dtrain,dvalid
-    df = pd.DataFrame(probdf)
-    df.columns = ['pred'+str(col) for col in df.columns]
-    trainall = trainall.join(df)
-
-    vdf = pd.DataFrame(vdf)
-    vdf.columns = ['pred' + str(col) for col in vdf.columns]
-    avalid = avalid.join(vdf)
-    df['label'] = y
-    trainall['label'] = y
-    avalid['label'] = vy
-    print trainall.head()
-    train = trainall[(trainall['clickTime'] >= 260000) & (trainall['clickTime'] < 280000)]
-    del trainall
-    import gc
-    for col in df.columns:
-        if col not in ['label']:
-            d = df[['label',col]]
-            print d.head()
-            d = d.groupby(col,as_index=False)['label'].mean()
-            d.rename(columns={'label':col+"mean_ratio"},inplace=True)
-            train = train.merge(d,on=col,how='left')
-            train.drop(col,axis=1,inplace=True)
-            avalid = avalid.merge(d, on=col, how='left')
-            avalid.drop(col,axis=1,inplace=True)
-            del d
-            gc.collect()
-    print train.head()
-    print train.shape
-
-    train.to_csv('../data/dup/train_xgbLRA.csv', index=None)
-    avalid.to_csv('../data/dup/valid_xgbLRA.csv', index=None)
 
 #predict()
 
